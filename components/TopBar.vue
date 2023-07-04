bdbd9768e721c14f1d2dc3361b66a872d0c5a3ef
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2023-07-05 00:59:11 +0200

    dev: init project + first component

---
+++ components/TopBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="top-bar">
+    <img class="top-bar__background" :src="backgroundImg" />
+    <div class="top-bar__container">
+      <img class="top-bar__profile" src="profile.jpg" />
+    </div>
+  </div>
+</template>
+
+<style>
+.top-bar__container {
+  position: relative;
+  max-width: 1200px;
+  height: 300px;
+  margin: 0 auto;
+  margin-top: -300px;
+}
+
+.top-bar__background {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+
+.top-bar__profile {
+  position: absolute;
+  top: 100px;
+  right: 0;
+  width: 300px;
+  height: auto;
+  border-radius: 100%;
+}
+</style>
+
+<script>
+export default {
+  name: 'TopBar',
+  props: {
+    backgroundImg: String,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <TopBar backgroundImg="code.jpg" />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
