73baa2f70d004a025898620740f4e141122e8bd5
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2023-07-06 10:58:58 +0200

    dev: test page + static css defined + values for paddings, margins and fonts defined

---
+++ pages/test.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>H1 header</h1>
+    <h2>H2 header</h2>
+    <h3>H3 header</h3>
+    <h4>H4 header</h4>
+    <h5>H5 header</h5>
+    <h6>H6 header</h6>
+    <p>
+      (default) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+      cursus dui vitae augue euismod, sed scelerisque velit suscipit. Quisque
+      viverra nulla leo, vitae hendrerit mi gravida vel. Ut quis ligula a magna
+      consequat tempus a at lorem. Etiam ultrices eu felis bibendum commodo.
+      Fusce faucibus quis enim eget rhoncus. Duis pretium rhoncus odio, et
+      malesuada urna mattis quis. Etiam consectetur eros ut tempor ornare. Sed a
+      erat at enim congue dignissim et et sapien.
+    </p>
+    <p style="font-size: 1.125rem">
+      (1.125rem) Maecenas tortor risus, eleifend eget finibus in, tristique
+      maximus velit. Etiam nec finibus tellus. Donec et ligula at arcu venenatis
+      venenatis. Sed congue ullamcorper orci, sed imperdiet ligula vehicula et.
+      Mauris hendrerit nisi eget nunc posuere scelerisque. Quisque condimentum a
+      lorem a feugiat. Vestibulum congue nisi ultrices ipsum ullamcorper
+      fermentum. Aenean sit amet vestibulum urna, sit amet vehicula ante. Morbi
+      pharetra ac libero sit amet sollicitudin. Vivamus in metus risus. Aenean
+      mauris quam, faucibus dignissim pellentesque eu, commodo id sapien. Nulla
+      quis ipsum elit. Vestibulum aliquam consequat cursus.
+    </p>
+    <p style="font-size: 1rem">
+      (1rem) Quisque ultricies, urna id consectetur auctor, ligula sapien
+      feugiat ipsum, et lacinia justo massa non velit. Fusce nec est elementum
+      enim ullamcorper egestas in sed quam. Donec faucibus velit placerat
+      molestie ultrices. Maecenas erat nunc, ultrices ac accumsan in,
+      condimentum in nibh. Praesent viverra est massa, ut cursus sapien tempor
+      a. Quisque condimentum malesuada interdum. Pellentesque sed interdum
+      ligula, in efficitur lectus. In diam nunc, iaculis eu auctor ac, rutrum
+      non lectus. Fusce euismod, erat eu tincidunt ultrices, est urna venenatis
+      dolor, non congue massa magna eget neque. Vivamus feugiat, risus ac rutrum
+      ultrices, ipsum arcu tincidunt velit, a luctus lorem mauris nec metus.
+      Suspendisse eu fringilla erat. Etiam vehicula elit et cursus varius. Proin
+      tellus sem, cursus eu hendrerit eu, convallis sit amet ante.
+    </p>
+    <p style="font-size: 0.75rem">
+      (0.75rem) Aenean consequat felis odio, sit amet interdum elit blandit at.
+      Sed bibendum orci elit, lacinia aliquam ante dictum quis. Donec lobortis,
+      eros a efficitur dictum, nulla neque laoreet nibh, nec feugiat eros nunc
+      eu diam. Sed ac dui vitae augue iaculis gravida. Quisque non dolor metus.
+      Nam efficitur sem neque, et blandit quam porttitor ornare. Phasellus sit
+      amet est nulla.
+    </p>
+    <p>
+      Margin values to keep: 4rem, 2rem, 1.5rem, 1.25rem, 1.125rem, 1rem, 0.5rem
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TestPage',
+})
+</script>
